a732ac47747507a7a37510e749f7f00afe7f563f
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-10-02 18:42:04 +0530

    creating observable from existing data - of method

--- index.ts
+++ index.ts
@@ -1,22 +1,25 @@
-import { Observable, Subscriber } from "rxjs";
+import { Observable, of, from, fromEvent } from "rxjs";
 import { allBooks, allReaders } from "./data";
 
 // An observable is not executed until an object subscribes to it.
-let allBooksObservable$ = Observable.create(subscriber => {
+// let allBooksObservable$ = Observable.create(subscriber => {
 
-  if (document.title !== 'RxBookTracker') {
-    subscriber.error(`Incorrect page title.`);
-  }
+//   if (document.title !== 'RxBookTracker') {
+//     subscriber.error(`Incorrect page title.`);
+//   }
 
-  for(let book of allBooks) {
-    subscriber.next(book);
-  }
+//   for(let book of allBooks) {
+//     subscriber.next(book);
+//   }
 
-  setTimeout(() => {
-    subscriber.complete();
-  }, 2000);
+//   setTimeout(() => {
+//     subscriber.complete();
+//   }, 2000);
 
-  return () => console.log(`Executing teardown code.`);
-});
+//   return () => console.log(`Executing teardown code.`);
+// });
 
-allBooksObservable$.subscribe(book => console.log(book.title));
+// allBooksObservable$.subscribe(book => console.log(book.title));
+
+let source1$ = of('hello', 10, true, allReaders[0].name);
+source1$.subscribe(value => console.log(value));
