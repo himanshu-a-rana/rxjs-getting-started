8b279272ca32b386ed67f5764f5cfb33b4d12b1f
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-10-02 21:16:42 +0530

    chaining operators

---
+++ ChainingOperators.ts
@@ -0,0 +1,18 @@
+import { of } from "rxjs";
+import { map, filter } from "rxjs/operators";
+// chaining operators
+
+let source$ = of(1, 2, 3, 4, 5);
+
+// Older syntax
+// source$
+//   .map(value => value * 2)
+//   .filter(mappedValue => mappedValue > 5)
+//   .subscribe(finalValue => console.log(finalValue));
+
+source$
+  .pipe(
+    map(value => value * 2),
+    filter(mappedValue => mappedValue > 5)
+  )
+  .subscribe(finalValue => console.log(finalValue)); // 6 8 10
